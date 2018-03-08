493b36a9e6e24092c2f9a97138618dedc3b97bb2
Burak Targaç <burak.targac@bilyoner.com>
2018-03-08 11:38:04 +0300

    fontawesome and jquery dependencies are added, layout improvements are made

--- src/renderer.js
+++ src/renderer.js
@@ -1,17 +1,63 @@
 import { ipcRenderer } from 'electron';
-// to tell webpack that we also need to process some styles
+import $ from 'jquery';
+
+// we also need to process some styles with webpack
+import fontawesome from '@fortawesome/fontawesome';
+import { faCloudUploadAlt } from '@fortawesome/fontawesome-free-solid'
+fontawesome.library.add(faCloudUploadAlt);
 import './styles/index.scss';
 
-document.addEventListener('drop', (event) => {
-  event.preventDefault();
-  event.stopPropagation();
+const drop = document.querySelector('input');
+const filesInput = document.querySelector('#files');
 
-  for (let file of event.dataTransfer.files) {
+const handleIn = () => {
+
+  $(".drop").css({
+    "border": "4px dashed #09f",
+    "background": "rgba(0, 153, 255, .05)"
+  });
+
+  $(".cont").css({
+    "color": "#09f"
+  });
+
+};
+
+const handleOut = () => {
+
+  $(".drop").css({
+    "border": "3px dashed #DADFE3",
+    "background": "transparent"
+  });
+
+  $(".cont").css({
+    "color": "#8E99A5"
+  });
+
+};
+
+const inEvents = ['dragenter', 'dragover'];
+const outEvents = ['dragleave', 'dragend', 'mouseout', 'drop'];
+
+inEvents.forEach(event => drop.addEventListener(event, handleIn));
+outEvents.forEach(event => drop.addEventListener(event, handleOut));
+
+const handleFileSelect = (event) => {
+  const files = event.target.files;
+
+  for (let file of files) {
+
+    // Only process excel files.
+    if (!file.type.match('officedocument.*')) {
+      continue;
+    }
+
     ipcRenderer.send('file-dropped', file.path);
   }
-});
 
-document.addEventListener('dragover', (event) => {
   event.preventDefault();
   event.stopPropagation();
-});
+
+};
+
+filesInput.addEventListener('change', handleFileSelect);
